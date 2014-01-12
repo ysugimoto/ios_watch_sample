b3e84fbab65224b566ed708f3cafe25d1b1510e2
sugimoto <sugimoto@wnotes.net>
2014-01-12 14:26:02 +0900

    Initial Commit

---
+++ WatchSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WatchSample
+//
+//  Created by sugimoto on 2014/01/12.
+//  Copyright (c) 2014年 Yoshiaki Sugimoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WatchSampleTests/WatchSampleTests.m
@@ -0,0 +1,34 @@
+//
+//  WatchSampleTests.m
+//  WatchSampleTests
+//
+//  Created by sugimoto on 2014/01/12.
+//  Copyright (c) 2014年 Yoshiaki Sugimoto. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WatchSampleTests : XCTestCase
+
+@end
+
+@implementation WatchSampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
